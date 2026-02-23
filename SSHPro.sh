c98e5a5875bd3c2ecf1c8d655f4c3994e6d33d15
DiegoBarreras <99460219+DiegoBarreras@users.noreply.github.com>
2026-02-23 18:54:52 +0100

    BASH Terminado

--- SSHPro.sh
+++ SSHPro.sh
@@ -9,6 +9,42 @@ verificar() {
 	fi
 }
 
+prenderservicio() {
+    local servicio=$1
+    echo "Habilitando el servicio $servicio:"
+    systemctl enable $servicio
+    systemctl start $servicio
+    echo -e "Servicio $servicio habilitado y activo.\n"
+}
+
+instalar() {
+	local paq=$1
+	echo "Buscando al paquete $paq:"
+	if rpm -q $paq &> /dev/null; then
+		echo -e "El paquete $paq fue instalado previamente.\n";
+		read -p "Deseas reinstalar el paquete $paq? s/n " res
+		res=${res,,}
+		if [[ $res == "s" ]]; then
+			echo -e "Reinstalando el paquete $paq.\n"
+			sudo dnf reinstall -y $paq
+			prenderservicio "sshd"
+		else
+			echo -e "La instalacion fue cancelada.\n"
+		fi
+	else
+		echo -e "El paquete $paq no ha sido instalado.\n"
+		read -p "Deseas instalar el paquete $paq? s/n " res
+		res=${res,,}
+		if [[ $res == "s" ]]; then
+			echo -e "Instalando el paquete $paq.\n"
+			sudo dnf install -y $paq
+			prenderservicio "sshd"
+		else
+			echo -e "La instalacion fue cancelada.\n"
+		fi
+	fi
+}
+
 if [[ $# -eq 0 ]]; then
 	echo -e "\n"
 	echo -e "---------------------------------------------"
@@ -16,10 +52,10 @@ if [[ $# -eq 0 ]]; then
 	echo -e "---------------------------------------------\n"
 
 	echo -e "Para verificar el estado del servicio:"
-	echo -e "./DNSPro.sh --verificar\n"
+	echo -e "./SSHPro.sh --verificar\n"
 
 	echo -e "Para re/instalar el paquete:"
-	echo -e "./DNSPro.sh --instalar\n"
+	echo -e "./SSHPro.sh --instalar\n"
 fi
 
 case $1 in
@@ -29,41 +65,8 @@ case $1 in
 	;;
 
 	--instalar)
-		instalar() {
-			local paq=$1
-			echo "Buscando al paquete $paq:"
-			if rpm -q $paq &> /dev/null; then
-				echo -e "El paquete $paq fue instalado previamente.\n";
-				flagSi=1
-			else
-				echo -e "El paquete $paq no ha sido instalado.\n"
-				flagSi=0
-			fi
-
-			if [[ $flagSi == "0" ]]; then
-				read -p "Deseas instalar el paquete $paq? s/n " res
-				res=${res,,}
-				if [[ $res == "s" ]]; then
-					echo -e "Instalando el paquete $paq.\n"
-					sudo dnf install -y $paq
-				else
-					echo -e "La instalacion fue cancelada.\n"
-				fi
-			else
-				read -p "Deseas reinstalar el paquete $paq? s/n " res
-				res=${res,,}
-				if [[ $res == "s" ]]; then
-					echo -e "Reinstalando el paquete $paq.\n"
-					sudo dnf reinstall -y $paq
-				else
-					echo -e "La instalacion fue cancelada.\n"
-				fi
-			fi 
-		}
-
 		echo -e "Re/Instalacion de Paquetes: \n"
-		instalar "bind"
-		instalar "bind-utils"
-		instalar "bind-doc"
+		instalar "openssh-server"
+		exit 0
 	;;
 esac
